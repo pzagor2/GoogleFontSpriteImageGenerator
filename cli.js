b68baf68af9bd0fcac1663216584e2a32bd3bd61
ginpei <ginpei@gmail.com>
2021-01-27 17:52:06 -0800

    update to make JSON file meanwhile

--- cli.js
+++ cli.js
@@ -1,3 +1,4 @@
+const { default: fetch } = require("node-fetch");
 const fs = require("fs/promises");
 const generator = require(".");
 const path = require("path");
@@ -7,8 +8,17 @@ const googleAPIKey = process.env.GOOGLE_API_KEY;
 main().catch((err) => console.error(err));
 
 async function main() {
-  const base64Data = await generateImageData();
-  await saveImage(base64Data);
+  const [base64Data, json] = await Promise.all([
+    generateImageData(),
+    fetchListJson(),
+  ]);
+  
+  const dir = await prepareDestDir();
+  await Promise.all([
+    saveImage(dir, base64Data),
+    saveJson(dir, json),
+  ]);
+
   console.log("Done.");
 }
 
@@ -19,32 +29,55 @@ async function main() {
 function generateImageData() {
   return new Promise((resolve) => {
     generator.getImage({
-      callback: function(base64Data) {
+      callback(base64Data) {
         resolve(base64Data);
       },
-      port: 1224,
+      googleAPIKey,
       options: {
         lineHeight: "25px",
         fontSize: "17px",
         width: "400px"
       },
-      googleAPIKey,
-    });  
+      port: 1224,
+    });
+  });
+}
+
+function fetchListJson() {
+  const url = `https://www.googleapis.com/webfonts/v1/webfonts?key=${googleAPIKey}`;
+  return fetch(url).then((res) => {
+    return res.text();
   });
 }
 
 /**
+ * @return {Promise<string>} Created dir name
+ */
+async function prepareDestDir() {
+  const listVersion = generateListVersion(new Date);
+  const dir = path.resolve(`dest/${listVersion}`);
+  await fs.mkdir(dir, { recursive: true });
+  return dir;
+}
+
+/**
+ * @param {string} dir
  * @param {string} base64Data
  * @returns {Promise<void>}
  */
-async function saveImage(base64Data) {
-  const listVersion = generateListVersion(new Date);
-  const destPath = path.resolve(`dest/${listVersion}/google-font-image.png`);
-
-  const dir = path.dirname(destPath);
-  await fs.mkdir(dir, { recursive: true });
-
+async function saveImage(dir, base64Data) {
+  const destPath = path.resolve(dir, "google-font-image.png");
   await fs.writeFile(destPath, base64Data, "base64");
+}
+
+/**
+ * @param {string} dir
+ * @param {string} json
+ * @returns {Promise<void>}
+ */
+async function saveJson(dir, json) {
+  const destPath = path.resolve(dir, "google-font-data.json");
+  await fs.writeFile(destPath, json, "utf8");
 }
 
 /**
